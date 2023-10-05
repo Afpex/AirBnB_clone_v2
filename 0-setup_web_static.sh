40d3cb805668a54bf831a88ba72f73f789673772
Anthony <bondolo90@gmail.com>
2023-10-05 13:32:27 -0700

    Compress before sending

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # sets up web servers for the deployment of web_static
-apt-get update -y > /dev/null 2>&1
-apt-get install nginx -y > /dev/null 2>&1
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get install nginx -y > /dev/null 2>&1
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Let us deploy web_static" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/^\tserver_name.*/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart > /dev/null 2>&1
+sudo service nginx restart > /dev/null 2>&1
