694d4d581041541c99325c1e4c463b3b5c7fccd0
Anthony <bondolo90@gmail.com>
2023-10-05 13:21:44 -0700

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,10 @@
 #!/usr/bin/env bash
-
-# Install Nginx if not already installed
-sudo apt-get update
-sudo apt-get install -y nginx
-
-# Create necessary folders if they don't exist
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-# Create a fake HTML file for testing
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n This is programming\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create or update symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration to serve the content of /data/web_static/current/
-# under the alias /hbnb_static
-sudo sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-# Restart Nginx to apply the changes
-sudo service nginx restart
+# sets up web servers for the deployment of web_static
+apt-get update -y > /dev/null 2>&1
+apt-get install nginx -y > /dev/null 2>&1
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Let us deploy web_static" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/^\tserver_name.*/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart > /dev/null 2>&1
